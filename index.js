e612ddf08838f4388d1c60b9268765c99a3c77ea
Tom-Rune Bornholdt <tr.bornholdt@hotmail.de>
2020-01-07 00:14:26 +0100

    Remove invalid firebase config

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 
 // TODO: Fill in your firebase project credentials here
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCPSkCCY6puRMIkVCB1mSIsGIyeIlqXaZM",
-  authDomain: "set-go.de",
-  databaseURL: "https://set-go-backend.firebaseio.com",
-  projectId: "set-go-backend",
-  storageBucket: "set-go-backend.appspot.com",
-  messagingSenderId: "651274672460",
-  appId: "1:651274672460:web:7753aef5183ce096"
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 });
 
 document.getElementById("loginBtn").addEventListener("click", () => {
